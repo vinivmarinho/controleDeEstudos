56bfa7a818764a1e5d2dd051aab8201df9d69e85
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-03-24 09:31:19 -0400

    refactor: Comenta o código

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
+// Cria a aplicação do servidor
 const app = express();
 
+// Importa as rotas
 const studyRoutes = require("./routes/studyRoutes");
 
+// Libera acesso de outros domínios (exemplo: React rodando em outra porta)
 app.use(cors());
+// Permite receber JSON no corpo da requisição
 app.use(express.json());
 
 // Rota principal
@@ -13,6 +17,7 @@ app.get("/", (req, res) => {
     res.send("API de controle está funcionando!");
 })
 
+// Conecta as rotas no app. Todas as rotas de studyRoutes irão começar com "/study"
 app.use("/study", studyRoutes);
 
 module.exports = app;
--- backend/src/routes/studyRoutes.js
+++ backend/src/routes/studyRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
+// Cria um roteador para organizar as rotas separadamente
 const router = express.Router();
-const studyController = require("..controllers/studyController");
+// Importa as funções que executam a lógica
+const studyController = require("../controllers/studyController");
 
+// Define as rotas
 router.get("/", studyController.getAll);
 router.post("/", studyController.create);
 router.put("/:id", studyController.update);
-router.delete("/:id", studyController.remove());
+router.delete("/:id", studyController.remove);
 
 module.exports = router;
